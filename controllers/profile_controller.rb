1c0921dd5e7543ec91762f3825a07bece08dc09b
Hari <hari@hari-virtual-machine.(none)>
2014-04-27 04:47:53 +0300

    profile form added

--- controllers/profile_controller.rb
+++ controllers/profile_controller.rb
@@ -1,10 +1,14 @@
 module HelpMeOut
 	class ProfileController < HelpMeOutBase
 		get "/" do
-			@member = find_current_member
-			haml :profile
+		  @member = find_current_user
+		  @items = Field.all
+	  	  haml :profile
 		end
-
-		helpers UserHelpers
+		post '/' do
+		  p params
+		  redirect '/user'
+		end
+		helpers UserHelpers, WebsiteHelpers, AuthenticationHelpers, ViewHelpers
 	end
 end
